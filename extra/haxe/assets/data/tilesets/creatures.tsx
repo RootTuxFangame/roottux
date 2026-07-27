139c09e400055118685aff12846d70b2024fc0f2
AnatolyStev <280393124+AnatolyStev@users.noreply.github.com>
2026-07-27 16:16:51 +0100

    Delete extra/haxe directory
    it was decided that this should be removed

--- extra/haxe/assets/data/tilesets/creatures.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="creatures" tilewidth="32" tileheight="32" tilecount="12" columns="12">
- <image source="../../images/editor/creatures.png" width="384" height="32"/>
-</tileset>
--- extra/haxe/assets/data/tilesets/tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="tiles" tilewidth="32" tileheight="32" tilecount="440" columns="20">
- <image source="../../images/tiles.png" width="640" height="704"/>
-</tileset>
